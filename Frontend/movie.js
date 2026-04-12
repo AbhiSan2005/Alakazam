e9feb90eb0ffdf2ecf6d1740fced059695a286fb
OnyX2811 <87792634+OnyX2811@users.noreply.github.com>
2026-04-12 16:28:42 +0530

    style: frotend changes'

--- Frontend/movie.js
+++ Frontend/movie.js
@@ -2,17 +2,31 @@
 const movieData = JSON.parse(sessionStorage.getItem("movieData"));
 
 if (movieData) {
-  // Update the DOM
-  document.getElementById("movie-title").textContent = movieData.mediaId;
-  document.getElementById("confidence").querySelector("span").textContent =
-    Math.round(movieData.confidence) + "%";
+  // Update the audio elements
+  document.getElementById("movie-title").textContent = movieData.finalDecision;
+  document
+    .getElementById("audio-confidence")
+    .querySelector("span").textContent =
+    Math.round(movieData.audioDetails.confidence) + "%";
 
-  // Optionally update the progress bar
-  const progressBar = document.querySelector(
-    "#confidence + div > div", // the inner div of the bar
+  const audioProgressBar = document.querySelector(
+    "#audio-confidence + div > div", // the inner div of the bar
   );
-  if (progressBar) {
-    progressBar.style.width = movieData.confidence + "%";
+  if (audioProgressBar) {
+    audioProgressBar.style.width = movieData.audioDetails.confidence + "%";
+  }
+
+  // Update the video elements
+  document
+    .getElementById("video-confidence")
+    .querySelector("span").textContent =
+    Math.round(movieData.videoDetails.confidence) + "%";
+
+  const videoProgressBar = document.querySelector(
+    "#video-confidence + div > div", // the inner div of the bar
+  );
+  if (videoProgressBar) {
+    videoProgressBar.style.width = movieData.videoDetails.confidence + "%";
   }
 } else {
   console.warn("No movie data found in sessionStorage!");
